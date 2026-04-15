941505b51c0a3732cde4425a330fe713dae7fbd5
Brian Ricardo Tamin <109451873+brii26@users.noreply.github.com>
2026-04-15 21:06:44 +0700

    Fix : Navbar gone
    fix -> bindingview

--- app/src/main/java/com/tit/nimonsapp/MainActivity.kt
+++ app/src/main/java/com/tit/nimonsapp/MainActivity.kt
@@ -9,11 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tit.nimonsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private lateinit var bottomNav: BottomNavigationView
 
     private val bottomNavDestinations =
         setOf(
@@ -25,24 +25,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        bottomNav = findViewById(R.id.bottom_nav)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        bottomNav.setupWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            bottomNav.visibility =
+            binding.bottomNav.visibility =
                 if (destination.id in bottomNavDestinations) {
                     View.VISIBLE
                 } else {
