d6c00a0c4d2a08e6a95846e3c5c10a933cf74380
Brian Ricardo Tamin <109451873+brii26@users.noreply.github.com>
2026-04-15 16:19:56 +0700

    Fix: fix button colour based on theme

--- app/src/main/java/com/tit/nimonsapp/ui/common/ButtonPrimaryView.kt
+++ app/src/main/java/com/tit/nimonsapp/ui/common/ButtonPrimaryView.kt
@@ -3,28 +3,9 @@ package com.tit.nimonsapp.ui.common
 import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.button.MaterialButton
-import com.tit.nimonsapp.R
 
 class ButtonPrimaryView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = com.google.android.material.R.attr.materialButtonStyle,
-) : MaterialButton(context, attrs, defStyleAttr) {
-
-    init {
-        context.theme.obtainStyledAttributes(attrs, R.styleable.ButtonPrimary, 0, 0).apply {
-            try {
-                if (hasValue(R.styleable.ButtonPrimary_buttonColor)) {
-                    val color = getColor(R.styleable.ButtonPrimary_buttonColor, 0)
-                    setBackgroundColor(color)
-                }
-                if (hasValue(R.styleable.ButtonPrimary_buttonTextColor)) {
-                    val textColor = getColor(R.styleable.ButtonPrimary_buttonTextColor, 0)
-                    setTextColor(textColor)
-                }
-            } finally {
-                recycle()
-            }
-        }
-    }
-}
+) : MaterialButton(context, attrs, defStyleAttr)
