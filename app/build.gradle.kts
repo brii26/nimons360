613dd91654bd76d30e107b06b17475c1f74b006d
Andrew Tedjapratama <andrewtedj@gmail.com>
2026-04-16 07:59:27 +0700

    deps: add dependency for map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,12 +52,15 @@ kotlin {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.3.0")
+    implementation(libs.maplibre.android.sdk)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
