8486e3d54dd0fe67c5b8316c00197b63c41891d3
Andrew Tedjapratama <andrewtedj@gmail.com>
2026-04-18 03:58:07 +0700

    fix: remove bottom nav space

--- app/src/main/java/com/tit/nimonsapp/MainActivity.kt
+++ app/src/main/java/com/tit/nimonsapp/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
