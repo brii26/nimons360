1b6432e5906fbf7e5a56ee25c1be5507cb45dc42
Brian Ricardo Tamin <109451873+brii26@users.noreply.github.com>
2026-04-17 19:57:56 +0700

    Chore : restructure bottom component 3 styles
    filled, outlined, destructive

--- app/src/main/java/com/tit/nimonsapp/ui/common/ButtonView.kt
+++ app/src/main/java/com/tit/nimonsapp/ui/common/ButtonView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.button.MaterialButton
 
-class ButtonPrimaryView @JvmOverloads constructor(
+class ButtonView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = com.google.android.material.R.attr.materialButtonStyle,
