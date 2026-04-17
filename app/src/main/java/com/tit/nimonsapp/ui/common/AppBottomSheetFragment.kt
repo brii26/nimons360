35a096672764d3ca842fb6ee6c5a9bf777e72182
Brian Ricardo Tamin <109451873+brii26@users.noreply.github.com>
2026-04-17 20:06:10 +0700

    feat : bottom sheet component for  xml

---
+++ app/src/main/java/com/tit/nimonsapp/ui/common/AppBottomSheetFragment.kt
@@ -0,0 +1,34 @@
+// Cara penggunaan:
+// class MySheet : AppBottomSheetFragment(R.layout.my_content_layout) {
+//     override fun onViewCreated(view: View, savedInstanceState: Bundle?) { ... }
+// }
+// MySheet().show(childFragmentManager, null)
+
+package com.tit.nimonsapp.ui.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.annotation.LayoutRes
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.tit.nimonsapp.R
+
+abstract class AppBottomSheetFragment(
+    @LayoutRes private val contentLayoutId: Int,
+) : BottomSheetDialogFragment() {
+
+    override fun getTheme(): Int = R.style.AppBottomSheet
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val root = inflater.inflate(R.layout.fragment_app_bottom_sheet, container, false)
+        val contentContainer = root.findViewById<FrameLayout>(R.id.bottom_sheet_content)
+        inflater.inflate(contentLayoutId, contentContainer, true)
+        return root
+    }
+}
